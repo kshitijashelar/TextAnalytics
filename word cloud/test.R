f7e85f5f5abcc47fc0dc7bd4b8048ea935da8e34
Kshitija Shelar <kshitija.shelar@gmsil.com>
2021-01-09 12:09:04 +0000

    Word cloud of text in R

---
+++ word cloud/test.R
@@ -0,0 +1,1 @@
+wordcloud("May our children and our children's children to a thousand generations, continue to enjoy the benefits conferred onus by united country, and have cause yet to rejoice under those glorious institutions bequeathed us by Washington and his compeers.",colors=brewer.pal(6,"Dark2"),random.order=FALSE)
